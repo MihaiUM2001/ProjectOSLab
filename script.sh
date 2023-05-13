e2d42ce3362fe50f28de81845730a9038c86ed38
MihaiUM2001 <mihai-alexandru.udrescu-milosav@student.upt.ro>
2023-05-13 14:48:58 +0300

    FinalWeek14 - secondREG with c 3 - script + test file

---
+++ script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]
+then
+    echo "Error: file does not exist!\n\n"
+    exit 1
+fi
+
+file=$1
+out=$(gcc -Wall -o ${file%.c} $file 2>&1)
+warning=$(echo "$out" | grep "warning:" | wc -l)
+error=$(echo "$out" | grep "error:" | wc -l)
+echo -e "Warnings: $warning, Errors: $error, File: $file"
---
+++ test.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    i = 3;
+    return 0;
+}
